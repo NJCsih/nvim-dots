614bf9b5ee3513aea3dea7f261e8cb1e7dce5120
NJCsih <NJCish@gmail.com>
2023-02-14 21:15:58 -0700

    Removed localConf and packer_compiled -- should not have been tracked in the first place

--- localConf.lua
+++
@@ -1,8 +0,0 @@
--- Device specific config, not tracked by git
-
--- Coloring
-vim.cmd.colorscheme("nord") -- set colorscheme
-vim.api.nvim_set_hl(0, "Normal", {bg = "none"}) -- add trasnparent background
-vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-
--- if undoddir needs to be set globally, per-device, do that here
